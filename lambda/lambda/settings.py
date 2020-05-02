38f3e7a76cd1541b7329d0b95bcaa308b12b4781
Rory Sullivan <codingrory@gmail.com>
2020-05-02 15:40:07 +0100

    Added login page.

--- lambda/lambda/settings.py
+++ lambda/lambda/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "users.apps.UsersConfig",
     "base.apps.BaseConfig",
     "django.contrib.admin",
     "django.contrib.auth",
