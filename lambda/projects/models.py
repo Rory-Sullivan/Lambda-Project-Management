e309c54feb1cb33dd85a4f1b192708cc64d138f3
Rory Sullivan <codingrory@gmail.com>
2020-05-08 11:31:18 +0100

    Project model created.

--- lambda/projects/models.py
+++ lambda/projects/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.shortcuts import reverse
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Project(models.Model):
+
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    # TODO: team_leader
+    team = models.ManyToManyField(User)
+
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modifed = models.DateTimeField(
+        verbose_name="Date and time project was last modifed", auto_now=True
+    )
+    date_due = models.DateField(blank=True)
+
+    completed = models.BooleanField(default=False)
+    date_completed = models.DateField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("project_detail", kwargs={"pk": self.pk})
