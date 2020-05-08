3991b617b7a927fa55f37c048b4e46eff40c672c
Rory Sullivan <codingrory@gmail.com>
2020-05-08 12:39:57 +0100

    Updated task model and reset database.

--- lambda/tasks/models.py
+++ lambda/tasks/models.py
@@ -1,14 +1,31 @@
 from django.db import models
 from django.shortcuts import reverse
 from django.contrib.auth.models import User
+from projects.models import Project
 
 
 class Task(models.Model):
+    PRIORITY_LEVELS = [
+        (3, "Low"),
+        (6, "Medium"),
+        (9, "High"),
+    ]
+
     title = models.CharField(max_length=100)
-    description = models.TextField()
-    rollover = models.BooleanField(default=False)
+    description = models.TextField(blank=True)
+    assigned_to_project = models.ForeignKey(
+        Project, on_delete=models.PROTECT, blank=True
+    )  # TODO: remove allow blank
+    assigned_to_user = models.ForeignKey(User, on_delete=models.PROTECT)
+    priority_level = models.IntegerField(choices=PRIORITY_LEVELS, default=3)
+    estimated_duration = models.DurationField()
+
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    date_due = models.DateField(blank=True)
+
     completed = models.BooleanField(default=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_completed = models.DateField(blank=True)
 
     def __str__(self):
         return self.title
