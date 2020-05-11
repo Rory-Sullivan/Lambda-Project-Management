0e24168ec393380173c2a250e62daf3b4f1ccfb5
Rory Sullivan <codingrory@gmail.com>
2020-05-11 17:04:33 +0100

    Made date due a necassary field. task#21

--- lambda/tasks/models.py
+++ lambda/tasks/models.py
@@ -20,7 +20,7 @@ class Task(models.Model):
 
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
-    date_due = models.DateField(null=True, blank=True)
+    date_due = models.DateField()
 
     completed = models.BooleanField(default=False)
     date_completed = models.DateField(null=True, blank=True)
