6fe8981df25a3c6638f206b5d819cc45cdee03be
Rory Sullivan <codingrory@gmail.com>
2020-05-11 14:24:04 +0100

    Updated task form. task#10

--- lambda/tasks/views.py
+++ lambda/tasks/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.contrib.auth import mixins
+from django.contrib.messages.views import SuccessMessageMixin
 from django.views import generic
 from .models import Task
+
+TASK_FIELDS = [
+    "title",
+    "description",
+    "assigned_to_project",
+    "assigned_to_user",
+    "priority_level",
+    "estimated_duration",
+    "date_due",
+]
 
 
 class TaskListView(mixins.LoginRequiredMixin, generic.ListView):
@@ -12,25 +23,28 @@ class TaskDetailView(mixins.LoginRequiredMixin, generic.DetailView):
     model = Task
 
 
-class TaskCreateView(mixins.LoginRequiredMixin, generic.CreateView):
-    model = Task
-    fields = ["title", "description", "rollover", "user"]
-
-
-class TaskUpdateView(mixins.LoginRequiredMixin, generic.UpdateView):
-    model = Task
-    fields = ["title", "description", "rollover", "user"]
-
-
-class TaskDeleteView(
-    mixins.LoginRequiredMixin, mixins.UserPassesTestMixin, generic.DeleteView
+class TaskCreateView(
+    mixins.LoginRequiredMixin, SuccessMessageMixin, generic.CreateView
 ):
     model = Task
+    fields = TASK_FIELDS
+    success_message = "Task #%(id) was created successfully"
+
+    def get_success_message(self, cleaned_data):
+        return self.success_message % dict(cleaned_data, id=self.object.id,)
+
+
+class TaskUpdateView(
+    mixins.LoginRequiredMixin, SuccessMessageMixin, generic.UpdateView
+):
+    model = Task
+    fields = TASK_FIELDS
+    success_message = "Task #%(id)s was updated successfully"
+
+    def get_success_message(self, cleaned_data):
+        return self.success_message % dict(cleaned_data, id=self.object.id,)
+
+
+class TaskDeleteView(mixins.LoginRequiredMixin, generic.DeleteView):
+    model = Task
     success_url = "/tasks"
-
-    def test_func(self):
-        task = self.get_object()
-
-        if self.request.user == task.user:
-            return True
-        return False
