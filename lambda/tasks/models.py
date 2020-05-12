03e05a5ddbe9bb2ad2346c0f402c1ce6f954e08b
Rory Sullivan <codingrory@gmail.com>
2020-05-12 12:09:08 +0100

    Added 'due in' count down. Task#15

--- lambda/tasks/models.py
+++ lambda/tasks/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.shortcuts import reverse
 from django.contrib.auth.models import User
 from projects.models import Project
+from datetime import date
 
 
 class Task(models.Model):
