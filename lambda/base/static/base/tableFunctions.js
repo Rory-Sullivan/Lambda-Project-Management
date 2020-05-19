658dff8c4577053c0b79893e7172c0ec87bb3ede
Rory Sullivan <codingrory@gmail.com>
2020-05-19 14:02:39 +0100

    Add search/filter functionality to list tables. task#22

--- lambda/base/static/base/tableFunctions.js
+++ lambda/base/static/base/tableFunctions.js
@@ -69,3 +69,43 @@ Array.from(sortHeaders).forEach(header => {
       }
   })
 })
+
+
+function searchTable(tableId, value) {
+  const filter = value.toLowerCase();
+  const table = document.getElementById(tableId);
+  const rows = table.getElementsByTagName("tr");
+
+  for (i = 1; i < rows.length; i++) {
+    const row = rows[i];
+    let cols = Array.from(row.getElementsByTagName("td"));
+    cols = cols.concat(Array.from(row.getElementsByTagName("th")));
+
+    let shouldHide = true;
+
+    cols.forEach(col => {
+      const text = col.innerText;
+      if (text.toLowerCase().indexOf(filter) > -1) {
+        shouldHide = false;
+      }
+    });
+
+    if (shouldHide) {
+      row.style.display = "none";
+    } else {
+      row.style.display = "";
+    }
+
+  }
+}
+
+const searchInputs = document.getElementsByClassName("searchInput");
+
+Array.from(searchInputs).forEach(input => {
+  input.addEventListener('keyup', (event) => {
+    const target = event.target;
+    const targetTableId = target.dataset.tableId;
+    const value = target.value;
+    searchTable(targetTableId, value);
+  });
+});
