57c567d0d29d5e22e537990a126f016f1cf83d99
Rory Sullivan <codingrory@gmail.com>
2020-05-28 10:33:27 +0100

    Fixed problems with duration widget. task#47

--- lambda/base/widgets.py
+++ lambda/base/widgets.py
@@ -1,4 +1,10 @@
 from django import forms
+import re
+
+HOUR_MINUTE = re.compile("^([0-1][0-9]|[2][0-3]):([0-5][0-9])$")
+HOUR_MINUTE_SECOND = re.compile(
+    "^([0-1][0-9]|[2][0-3]):([0-5][0-9]):([0-5][0-9])$"
+)
 
 
 class DateWidget(forms.TextInput):
@@ -9,6 +15,10 @@ class DurationWidget(forms.TextInput):
     input_type = "time"
 
     def value_from_datadict(self, data, files, name):
-        raw_value = super().value_from_datadict(data, files, name)
-        (hours, minutes) = raw_value.split(":")
-        return f"00 {hours}:{minutes}:00.0"
+        time_val = super().value_from_datadict(data, files, name)
+
+        if HOUR_MINUTE.match(time_val):
+            return time_val + ":00"
+        if HOUR_MINUTE_SECOND.match(time_val):
+            return time_val
+        return None
