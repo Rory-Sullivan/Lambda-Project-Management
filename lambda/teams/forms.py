fff658b4575a844ec5d080b1e81690a6b40a33d3
Rory Sullivan <codingrory@gmail.com>
2020-06-02 22:41:03 +0100

    Team leader must be in team. task#18

--- lambda/teams/forms.py
+++ lambda/teams/forms.py
@@ -11,3 +11,15 @@ class TeamForm(forms.ModelForm):
             "leader",
             "members",
         ]
+
+    def clean(self):
+        cleaned_data = super().clean()
+        leader = cleaned_data.get("leader")
+        members = cleaned_data.get("members")
+
+        if leader not in members.all():
+            msg = f"""
+                The team leader must be included as a member of the team.
+                Please include {leader} in the members section below.
+            """
+            self.add_error("leader", msg)
