860b5222ba080feea17a843eeba879a3305971a0
Rory Sullivan <codingrory@gmail.com>
2020-06-04 09:44:39 +0100

    Created manager status for users. task#28

--- lambda/users/models.py
+++ lambda/users/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-    is_team_leader = models.BooleanField(default=False)
+    is_manager = models.BooleanField(default=False)
 
     def __str__(self):
         return self.user.username
