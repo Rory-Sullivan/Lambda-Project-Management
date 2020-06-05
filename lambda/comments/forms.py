db1e09640ae25369bc3d8ab804b1861f3037a0e9
Rory Sullivan <codingrory@gmail.com>
2020-06-05 18:40:34 +0100

    Made comments box smaller. task#9

--- lambda/comments/forms.py
+++ lambda/comments/forms.py
@@ -6,9 +6,11 @@ class TaskCommentForm(forms.ModelForm):
     class Meta:
         model = models.TaskComment
         fields = ["text"]
+        widgets = {"text": forms.Textarea(attrs={"cols": 80, "rows": 5})}
 
 
 class ProjectCommentForm(forms.ModelForm):
     class Meta:
         model = models.ProjectComment
         fields = ["text"]
+        widgets = {"text": forms.Textarea(attrs={"cols": 80, "rows": 5})}
