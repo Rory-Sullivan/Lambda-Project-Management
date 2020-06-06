9b900cb2fb8b858b810108b49a967e7f2802350b
Rory Sullivan <codingrory@gmail.com>
2020-06-06 11:51:52 +0100

    Added demo user flag to user profile.

--- lambda/users/models.py
+++ lambda/users/models.py
@@ -7,6 +7,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     is_manager = models.BooleanField(default=False)
+    is_demo_user = models.BooleanField(default=False)
 
     def __str__(self):
         return self.user.username
