8ed84de58657d12972a16a53d6d154ab5ed5ef1d
Rory Sullivan <codingrory@gmail.com>
2020-06-11 12:24:16 +0100

    Team, project and task automatically created when  a new user registers. task#43

--- lambda/users/signals.py
+++ lambda/users/signals.py
@@ -1,6 +1,13 @@
+from datetime import date, timedelta
+
+from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.contrib.auth.models import User
+
+from projects.models import Project
+from tasks.models import Task
+from teams.models import Team
+
 from .models import Profile
 
 
@@ -8,6 +15,39 @@ from .models import Profile
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+
+        team = Team.objects.create(
+            name="Just me",
+            leader=instance,
+            created_by=instance,
+            modified_by=instance,
+        )
+        team.members.set([instance])
+
+        TODO_DESCRIPTION = "Your to do list."
+        project = Project.objects.create(
+            name="To do",
+            description=TODO_DESCRIPTION,
+            team=team,
+            created_by=instance,
+            modified_by=instance,
+        )
+
+        TASK_DESCRIPTION = """If you you are seeing this message you have
+            obviously already registered for lambda and so you can go ahead and
+            complete this task by hitting the button below.
+        """
+        Task.objects.create(
+            title="Register for Lambda",
+            description=TASK_DESCRIPTION,
+            project=project,
+            team=team,
+            assigned_to=instance,
+            estimated_duration=timedelta(0, 120, 0),
+            created_by=instance,
+            modified_by=instance,
+            date_due=date.today(),
+        )
 
 
 @receiver(post_save, sender=User)
