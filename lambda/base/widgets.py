8cd7b85ed36417170928da99857ce02c049d1beb
Rory Sullivan <codingrory@gmail.com>
2020-06-11 16:42:58 +0100

    Duration widget updated. task#48

--- lambda/base/widgets.py
+++ lambda/base/widgets.py
@@ -1,24 +1,52 @@
-from django import forms
-import re
+from datetime import timedelta
 
-HOUR_MINUTE = re.compile("^([0-1][0-9]|[2][0-3]):([0-5][0-9])$")
-HOUR_MINUTE_SECOND = re.compile(
-    "^([0-1][0-9]|[2][0-3]):([0-5][0-9]):([0-5][0-9])$"
-)
+from django import forms
 
 
 class DateWidget(forms.TextInput):
     input_type = "date"
 
 
-class DurationWidget(forms.TextInput):
-    input_type = "time"
+class DurationWidget(forms.MultiWidget):
+    template_name = "base/widgets/duration_widget.html"
+
+    def __init__(self, attrs=None):
+        HOUR_MIN = 0
+        HOUR_MAX = 23
+        MINUTE_MIN = 0
+        MINUTE_MAX = 59
+        hour_attrs = {
+            "min": HOUR_MIN,
+            "max": HOUR_MAX,
+            "placeholder": "hh",
+            "class": "form-control",
+        }
+        minute_attrs = {
+            "min": MINUTE_MIN,
+            "max": MINUTE_MAX,
+            "placeholder": "mm",
+            "class": "form-control",
+        }
+        if attrs:
+            hour_attrs.update(attrs)
+            minute_attrs.update(attrs)
+
+        widgets = [
+            forms.NumberInput(attrs=hour_attrs),
+            forms.NumberInput(attrs=minute_attrs),
+        ]
+        super().__init__(widgets, attrs)
+
+    def decompress(self, value):
+        if isinstance(value, timedelta):
+            return [value.hours, value.minutes]
+
+        elif isinstance(value, str):
+            hours, minutes, _ = value.split(":")
+            return [hours, minutes]
+
+        return [None, None]
 
     def value_from_datadict(self, data, files, name):
-        time_val = super().value_from_datadict(data, files, name)
-
-        if HOUR_MINUTE.match(time_val):
-            return time_val + ":00"
-        if HOUR_MINUTE_SECOND.match(time_val):
-            return time_val
-        return None
+        hours, minutes = super().value_from_datadict(data, files, name)
+        return f"{hours}:{minutes}:00"
