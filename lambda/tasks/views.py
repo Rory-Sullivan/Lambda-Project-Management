27726511fde862dbed529fc4e73cf0b51eded784
Mailhot <36643298+Mailhot@users.noreply.github.com>
2020-11-10 10:55:05 -0500

    Show the unassigned task for team members and team leader (was team leader only)

--- lambda/tasks/views.py
+++ lambda/tasks/views.py
@@ -55,7 +55,7 @@ class UnassignedTaskListView(mixins.LoginRequiredMixin, generic.ListView):
     def get_queryset(self):
         q = Task.objects.none()
 
-        for project in Project.objects.filter(team__leader=self.request.user):
+        for project in (Project.objects.filter(team__leader=self.request.user) | Project.objects.filter(team__members__in=[self.request.user])):
             q = q | project.task_set.filter(assigned_to=None)
 
         return q.distinct().order_by("date_due", "-priority_level")
